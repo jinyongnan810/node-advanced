a7e75969c3f18d4917f976f95ba118196ff77438
Yuunan Kin <yuunan.kin@gmail.com>
2021-04-04 10:35:37 +0900

    node thread pool

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Node bases on V8(execute javascript code) and livuv(native functions)
 - [Example](https://github.com/nodejs/node/blob/master/lib/internal/crypto/pbkdf2.js)
+- Node's event-loop is single threaded. But inside the libuv, there is a thread pool to be used by some framework to support things like callbacks.
---
+++ thread.js
@@ -0,0 +1,37 @@
+const crypto = require("crypto");
+// libuv manages a thread pool that defaultly has 4 threads
+// const start1 = Date.now();
+// crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+//   console.log("1:", Date.now() - start1);
+// });
+// crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+//   console.log("2:", Date.now() - start1);
+// });
+// crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+//   console.log("3:", Date.now() - start1);
+// });
+// crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+//   console.log("4:", Date.now() - start1);
+// });
+// crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+//   console.log("5:", Date.now() - start1);
+// });
+
+// change thread pool size, but the result is not very comprehensible
+process.env.UV_THREADPOOL_SIZE = 6;
+const start2 = Date.now();
+crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+  console.log("1:", Date.now() - start2);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+  console.log("2:", Date.now() - start2);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+  console.log("3:", Date.now() - start2);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+  console.log("4:", Date.now() - start2);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "SHA512", () => {
+  console.log("5:", Date.now() - start2);
+});
