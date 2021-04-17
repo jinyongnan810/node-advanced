1f233047b100ea027d2f454452453b79d4754446
Yuunan Kin <yuunan.kin@gmail.com>
2021-04-17 10:59:08 +0900

    try block the event loop for a while

--- 2.enhance-perfomance/index.js
+++ 2.enhance-perfomance/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
+const blockEventLoopForAWhile = async (duration) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+app.get("/", async (req, res) => {
+  await blockEventLoopForAWhile(10000);
   res.send("hello");
 });
 
